6270e20c1570245ba6c414339506479724280721
Michael L Petersen <ekmlpe@danskespil.dk>
2018-05-01 08:50:53 +0200

    injecting global styles were weird.. fixed

--- src/layouts/global-styles.js
+++ src/layouts/global-styles.js
@@ -2,22 +2,5 @@ import {injectGlobal} from 'styled-components'
 
 injectGlobal`
 
-* {box-sizing: border-box;}
 
-  body {
-    margin: 0;
-    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif;
-    font-size: 0.8rem;
-    font-weight: 400;
-    line-height: 1.7;
-    color: #ccc;
-    text-align: left;
-    background-color: #1e1e1e;
-  }
-
-  .container {
-    max-width: 70rem;
-    margin: 0 auto;
-    padding: 0 1rem;
-  }
 `;
