b1c4c722913c3f3c681ade79f432538705c76e8c
Mike Lothar <mikelothar@gmail.com>
2018-06-01 21:25:54 +0200

    better animated background

--- src/components/AnimatedBg/index.js
+++ src/components/AnimatedBg/index.js
@@ -9,14 +9,13 @@ injectGlobal `
 `
 const Bg = styled.div `
   position: absolute;
-  top: 3.25rem;
+  top: 0;
   left: 0;
   bottom: 0;
   right: 0;
   background: no-repeat center center;
   background-size: cover;
   opacity: 0.2;
-  opacity: 1;
   background-image: url(${props => props.bg
   ? require('../../../assets/animated-bg/' + props.bg)
   : require('../../../assets/animated-bg/cat-typing.gif')});
